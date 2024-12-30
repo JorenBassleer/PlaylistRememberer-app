4a414debae502f80d8443570ef3998f25c32f50b
Joren Bassleer <jorenbassleer1997@gmail.com>
2024-12-30 22:16:09 +0100

    start auth

--- src/stores/base.js
+++ src/stores/base.js
@@ -1,13 +1,12 @@
 import { defineStore } from 'pinia';
 
 const useBaseStore = defineStore('base', () => {
-  const apiTest = async () => {
-    const response = await fetch(`${import.meta.env.VITE_API_URL}/video`);
-    // eslint-disable-next-line no-console
-    if (!response.ok) console.log('resposne:', response);
+  const getLoginUrl = async () => {
+    const response = await fetch(`${import.meta.env.VITE_API_URL}/google`);
+
     return response.json();
   };
-  return { apiTest };
+  return { getLoginUrl };
 });
 
 export default useBaseStore;
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,46 @@
 <template>
-  <div class="bg-blue-500">
-    Home
-  </div>
+  <div class="flex justify-center items-center h-screen">
+      <div
+        class="border bg-white rounded p-4 text-center text-gray-600 font-semibold cursor-pointer"
+        @click="onLogin"
+      >
+        Login
+      </div>
+    </div>
 </template>
 <script setup>
+import { useRouter } from 'vue-router';
+import useBaseStore from '@/stores/base';
+
+const { getLoginUrl } = useBaseStore();
+
+const onLogin = async () => {
+  const url = await getLoginUrl();
+  window.location.href = url;
+};
+
+const handleCallback = async () => {
+  const router = useRouter();
+  const queryParams = new URLSearchParams(window.location.search);
+  const code = queryParams.get('code');
+  const state = queryParams.get('state');
+
+  if (code && state) {
+    try {
+      const response = await fetch(`${import.meta.env.VITE_API_URL}/google/oauth2callback`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ code, state }),
+      });
+
+      const data = await response.json();
+      router.push('/dashboard');
+    } catch (error) {
+      console.error('Error during token exchange:', error);
+    }
+  }
+};
+
+// Call this function on the front-end's callback route
+handleCallback();
 </script>

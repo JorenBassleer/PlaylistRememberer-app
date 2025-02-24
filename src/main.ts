8831e9c30a8157b8b582b24c84f6b0e6a1c3adbd
Joren Bassleer <jorenbassleer1997@gmail.com>
2025-02-24 22:00:16 +0100

    add vue query

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
+import setupVueQuery from './plugins/vueQuery';
 
 import App from './App.vue';
 import router from './router';
@@ -7,6 +8,8 @@ import router from './router';
 import './assets/main.css';
 
 const app = createApp(App);
+
+setupVueQuery(app);
 
 app.use(createPinia());
 app.use(router);
